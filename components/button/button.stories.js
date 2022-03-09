d1603b3c790c088090b3189867f07f78916f30cd
Max <sidorzh@gmail.com>
2022-03-09 15:59:59 +0100

    add storybook

---
+++ components/button/button.stories.js
@@ -0,0 +1,6 @@
+import "./button.css";
+export default {
+  title: "Components/button 123",
+};
+export const Default = () => `<button class="button">Click me</button>`;
+export const Big = () => `<button class="button button--big">Click me</button>`;
---
+++ components/main_img_style/main_img.stories.js
@@ -0,0 +1,6 @@
+import "./main_img.css";
+export default {
+  title: "Components/main_img",
+};
+export const IMG = () =>
+  `<img class="main_img_styling" src="https://www.politico.eu/cdn-cgi/image/width=1320,height=881,fit=crop,quality=80,format=auto,onerror=redirect/wp-content/uploads/2022/03/04/Live-Blog-Ukraine-F.jpg"/>`;
